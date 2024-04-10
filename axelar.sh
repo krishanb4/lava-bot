f12a9af51a0f6862b82a559efa736d517bf82ef8
Krishan Bandara <34151165+krishanb4@users.noreply.github.com>
2024-04-10 19:45:14 +0530

    Update axelar.sh

--- axelar.sh
+++ axelar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://tm.axelar.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://tm.axelar.lava.build/lava-referer-c25ea0d0-0f4d-428d-ad75-67a617b02660/"
 
 OUTPUT_DIR="logs"
 
